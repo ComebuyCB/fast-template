53e774b330e84d77e012b17cfd9f6e5e1841e388
ComebuyCB <ben2000200066@yahoo.com.tw>
2023-07-11 15:26:11 +0800

    css desc

--- BS5+JQ3+FA5+Vue3/static/common/js/main.js
+++ BS5+JQ3+FA5+Vue3/static/common/js/main.js
@@ -1,8 +1,8 @@
 set100vh()
 window.onresize = function(){
-  set100vh()
+	set100vh()
 }
 
 function set100vh(){
-  document.documentElement.style.setProperty('--100vh', window.innerHeight + 'px' )
+	document.documentElement.style.setProperty('--100vh', window.innerHeight + 'px' )
 }
